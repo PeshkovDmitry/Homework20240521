bd91e34116f349f00396ff86357c73a93acb0737
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-04-24 23:23:28 +0300

    Добавлен book-service

---
+++ book-service/src/test/java/ru/gb/bookservice/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
