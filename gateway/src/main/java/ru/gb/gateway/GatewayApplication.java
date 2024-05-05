0c9ec73d2a9de4ddd7be69ac73ddd13d42e658eb
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-05-05 20:58:47 +0300

    Пытаюсь настроить Zuul

--- gateway/src/main/java/ru/gb/gateway/GatewayApplication.java
+++ gateway/src/main/java/ru/gb/gateway/GatewayApplication.java
@@ -2,16 +2,18 @@ package ru.gb.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+
 @SpringBootApplication
-//@EnableZuulProxy
+@EnableZuulProxy
 
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+
+
 
 }
